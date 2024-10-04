97fde17237cd150940dce33f1607d6f8fb7a7510
PolaNagaSai <akunuri.9sai@gmail.com>
2024-10-04 18:24:22 +0530

    Revert "movies app"
    This reverts commit 72a9f947af2e4c9b62554eff59372cb05d54da2e.

--- src/store/Store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import moviezReducer from './MoviezSlice'
-
-export const store = configureStore({
-  reducer: {
-    moviezData:moviezReducer
-  },
-})
